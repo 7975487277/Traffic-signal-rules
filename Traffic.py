185353e300480ee97c83654af933c88d2dc4a487
7975487277 <sribalajibarapati95@gmail.com>
2026-07-15 21:33:57 +0530

    Create Traffic.py
    This program is done to learn the basic traffic light rules by entering the user exact traffic light colour it will say the answer . Is the colour enter is wrong then it will say invalid colour

---
+++ Traffic.py
@@ -0,0 +1,9 @@
+signal = input("Enter the signal ")
+if signal = "Red ":
+  print("stop")
+elif signal = "Yellow":
+  print("ready")
+elif signal = "Green":
+  print("Go")
+else:
+  print("invalid colour")
